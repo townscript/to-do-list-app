591ba5022996c87c2b62512a10b3ff718e2cf7ff
Sachin Sharma <sachin@townscript.com>
2015-09-08 17:07:29 +0530

    Update TagService.java

--- src/main/java/com/townscript/todo/service/tag/TagService.java
+++ src/main/java/com/townscript/todo/service/tag/TagService.java
@@ -3,6 +3,8 @@ package com.townscript.todo.service.tag;
 import com.townscript.todo.model.Tag;
 
 public interface TagService {
+	
+	// As mentioned in another comment, no "Public" is required here
 	public int addTag(Tag tag);
 	public void addExistingTagtoTask(Tag tag,int taskid);
 	public void deleteTag(int tagid);
