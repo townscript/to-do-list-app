ee152370b95e18032f0c62c68c7507789dd269a8
Sachin Sharma <sachin@townscript.com>
2015-09-08 16:38:12 +0530

    Update CategoryDao.java
    For code review 1

--- src/main/java/com/townscript/todo/dao/category/CategoryDao.java
+++ src/main/java/com/townscript/todo/dao/category/CategoryDao.java
@@ -2,9 +2,14 @@ package com.townscript.todo.dao.category;
 
 import com.townscript.todo.model.Category;
 
+
 public interface CategoryDao {
+	
+	// No need to use "Public" here, as by default Interface declared methods are public
 	public int addCategory(Category category);
 	public void removeCategory(int categoryid);
+	
+	// For clearity of reading, we should use CamelCase, it should be categoryId
 	public Category readCategory(int categoryid);
 	public void updateCategory(Category category);
 	public Category getCategoryofTask(int taskid);
